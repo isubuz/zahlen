e8ca60632506dfd151d72916efce152b0c51fa99
Subhajit Ghosh <i.subhajit.ghosh@gmail.com>
2014-03-07 14:15:22 +0000

    Implemented all methods and added documentation

--- ds/tree/fenwick_tree.py
+++ ds/tree/fenwick_tree.py
@@ -1,34 +1,79 @@
-__author__ = 'isubuz'
+# -*- coding: utf-8 -*-
+
+"""
+    zahlen.ds.tree.fenwick_tree
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This module implements the Fenwick tree (also known as Bit Indexed Tree)
+    data structure.
+
+    :copyright: (c) 2014 by Subhajit Ghosh.
+    :license: MIT, see LICENSE for more details.
+"""
 
 
 class FenwickTree(object):
-    def __init__(self, frequencies):
-        # Note that the Fenwick tree is represented as a one-indexed array.
-        self.size = len(frequencies) + 1
-        self._tree = [0] * self.size
+    """Construct a Fenwick tree.
 
-        for i in range(1, self.size + 1):
-            self.update(i, frequencies[i - 1])
+    This implementation of the Fenwick Tree uses a fixed length array to
+    represent the tree. The size of the tree is specified during the
+    construction of the tree and cannot be modified i.e. new elements cannot be
+    added but existing elements can be modified.
+
+    Example usage::
+        # Construct the tree
+        ft = FenwickTree(10)
+
+        # Update an element at an index
+        ft.update(index, value)
+
+        # Read the value at an index
+        ft.read(index)
+    """
+
+    def __init__(self, size):
+        """
+        :param size: no. of elements to be contained in the tree
+        """
+
+        if size < 1:
+            raise ValueError('Size must be greater than 0')
+
+        self._size = size
+        self._tree = [0] * self._size
+
+    @property
+    def size(self):
+        return self._size
 
     def read(self, index):
         """Read the value at the specified index.
 
-        Arguments:
-            index   position in an one-indexed array
+        As the tree is implemented using a fixed length array, the index must be
+        based on a 0-indexed array.
         """
+
+        if not 0 <= index <= self._size - 1:
+            raise IndexError('Invalid index: {0}'.format(index))
+
         value = 0
+        index += 1  # position w.r.t a one-indexed array.
         while index > 0:
-            value += self._tree[index]
+            value += self._tree[index - 1]
             index -= (index & -index)
         return value
 
     def update(self, index, value):
         """Update the value at the specified index.
 
-        Arguments:
-            index   position in an one-indexed array
-            value   new value at the specified index
+        As the tree is implemented using a fixed length array, the index must be
+        based on a 0-indexed array.
         """
-        while index <= self.size:
-            self._tree[index] += value
+
+        if not 0 <= index <= self._size - 1:
+            raise IndexError('Invalid index: {0}'.format(index))
+
+        index += 1  # position w.r.t a one-indexed array.
+        while index <= self._size:
+            self._tree[index - 1] += value
             index += (index & -index)
