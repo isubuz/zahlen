c4de82770428a81d3cf2fddae0241698e4ddead5
Subhajit Ghosh <i.subhajit.ghosh@gmail.com>
2014-03-11 08:50:30 +0000

    Added new tests for Minmax heap construction.

--- tests/ds/tree/test_min_max_heap.py
+++ tests/ds/tree/test_min_max_heap.py
@@ -45,8 +45,44 @@ class TestBuildMinMaxHeap(unittest.TestCase):
         mmh = MinMaxHeap([2, 1, 4, 3, 5, 2, -1, 7, 3, 0])
         self.assertListEqual(mmh.elements, [-1, 7, 4, 2, 1, 2, 0, 3, 3, 5])
 
-    # Grandchild - repl (with(out) trickle down at min/max level)
-    # Child - repl (min/max leveL)
+    def test_swap_child_parent_min_level(self):
+        """Swap 0 with its parent 3 which is at a min level."""
+        mmh = MinMaxHeap([2, 1, 4, 3, 5, 2, -1, 7, 0, 6])
+        self.assertListEqual(mmh.elements, [-1, 7, 4, 2, 1, 2, 0, 5, 3, 6])
+
+    def test_swap_child_parent_max_level(self):
+        """Swap 3 with its parent 1 which is at a max level."""
+        mmh = MinMaxHeap([2, 1, 6, 3, 2, 2, -1, 5, 4])
+        self.assertListEqual(mmh.elements, [-1, 5, 6, 2, 2, 2, 1, 3, 4])
+
+    def test_swap_grandchild_grandparent_min_level(self):
+        """Swap -1 with its grand parent 2 which is at a max level."""
+        mmh = MinMaxHeap([2, 4, 6, 2, 3, 4, -1, 4])
+        self.assertListEqual(mmh.elements, [-1, 4, 6, 2, 3, 4, 2, 4])
+
+    def test_swap_grandchild_grandparent_max_level(self):
+        """Swap 5 with its grand parent 4 which is at a max level."""
+        mmh = MinMaxHeap([2, 4, 6, 2, 3, 4, -1, 5])
+        self.assertListEqual(mmh.elements, [-1, 5, 6, 2, 3, 4, 2, 4])
+
+    def test_swap_grandchild_trickle_down_min_level(self):
+        """Swap 1 with its grand parent 4 which is at a min level. 4 is then
+        trickled down and swapped with its child 2.
+        """
+        mmh = MinMaxHeap([4, 8, 4, 1, 5, 2, -1, 7, 2, 6, 8, 1, 4, 5, 6, 6, 4, 2,
+                          1])
+        self.assertListEqual(mmh.elements, [-1, 8, 6, 1, 5, 1, 1, 7, 4, 6, 8,
+                                            2, 4, 4, 5, 6, 4, 2, 2])
+
+    def test_swap_grandchild_trickle_down_max_level(self):
+        """Swap 7 with its grand parent 5 which is at a max level. 5 is then
+        trickled down and swapped with 6.
+        """
+        mmh = MinMaxHeap([2, 5, 4, 3, 5, 2, -1, 7, 6, 6, 8, 1, 4, 5, 3, 6, 4, 2,
+                          0])
+        self.assertListEqual(mmh.elements, [-1, 8, 5, 2, 5, 1, 0, 6, 6, 6, 7, 2,
+                                            4, 4, 3, 5, 4, 3, 2])
+
 
 if __name__ == '__main__':
     unittest.main()
